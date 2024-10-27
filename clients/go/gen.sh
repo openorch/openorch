e4315854bbc056af964b15776fa7b1ef3c959e10
crufter <dobronszki@gmail.com>
2024-10-27 06:47:43 +0100

    fix mock generation

--- clients/go/gen.sh
+++ clients/go/gen.sh
@@ -4,16 +4,18 @@ set -e
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-LOCALTRON_DIR="$SCRIPT_DIR/../../server"
+SERVER_DIR="$SCRIPT_DIR/../../server"
 
-cd "$LOCALTRON_DIR"
+cd "$SERVER_DIR"
 swag init --parseDependency
 
 cd "$SCRIPT_DIR"
 rm -rf *.go
-openapi-generator-cli generate -i "$LOCALTRON_DIR/docs/swagger.yaml" -g go -o . --additional-properties generateInterfaces=true
+openapi-generator-cli generate -i "$SERVER_DIR/docs/swagger.yaml" -g go -o . --additional-properties generateInterfaces=true
 rm -rf api docs go.mod
 cp go.mod.template go.mod
+
+bash "$SERVER_DIR/mock_go.sh"
 
 # A hack to fix gopls because it doesn't like lots of client files being regenerated
 killall gopls || true
