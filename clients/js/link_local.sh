d30ea226041a971be83e35fa49c4f2541b60112c
crufter <dobronszki@gmail.com>
2024-12-16 09:14:42 +0100

    fix

--- clients/js/link_local.sh
+++ clients/js/link_local.sh
@@ -2,20 +2,12 @@
 
 set -e
 
-if ! command -v rollup > /dev/null; then
-  npm install -g rollup
-else
-  echo "Rollup is already installed."
-fi
-
 cd client;
-npm run build;
 npm link;
 cd ..
 
 cd example;
 npm link @openorch/client;
-npm run build
 cd ..
 
 cd ../../desktop/workspaces/angular-app/
