3744700ab8f31ba3286e6ddd380881e437845279
crufter <dobronszki@gmail.com>
2025-02-21 11:51:53 +0100

    fix

--- server/main.go
+++ server/main.go
@@ -29,7 +29,7 @@ import (
 var port = router.GetPort()
 
 // @title           OpenOrch
-// @version         0.3.0-rc.22
+// @version         0.3.0-rc.23
 // @description     A language-agnostic microservices framework for building AI applications.
 // @termsOfService  http://swagger.io/terms/
 
