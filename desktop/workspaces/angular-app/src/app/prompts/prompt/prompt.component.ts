f083995b7d25e17202f7239deb9f6b9a653d64cb
crufter <dobronszki@gmail.com>
2025-02-28 14:21:34 +0100

    feat: display parameters and engineParameters in the prompt list

--- desktop/workspaces/angular-app/src/app/prompts/prompt/prompt.component.ts
+++ desktop/workspaces/angular-app/src/app/prompts/prompt/prompt.component.ts
@@ -36,5 +36,18 @@ export class PromptComponent {
 	@Input() prompt!: Prompt;
 	@Input() expanded = false;
 
+	parameters = '';
+
 	constructor() {}
+
+	ngOnInit() {
+		this.parameters = JSON.stringify(
+			{
+				parameters: this.prompt.parameters,
+				engineParameters: this.prompt.engineParameters,
+			},
+			null,
+			2
+		);
+	}
 }
